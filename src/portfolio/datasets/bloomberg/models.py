e5dcfad600c16aa00027bcc5a061bd3b99e1ea22
GMPCODE <giovannino.bobs@gmail.com>
2025-02-21 18:35:39 +0000

    initialization

--- src/portfolio/datasets/bloomberg/models.py
+++
@@ -1,6 +0,0 @@
-from utils.utils import EnhancedStrEnum
-
-
-class SourceType(EnhancedStrEnum):
-    CSV: str = "csv"
-    EXCEL: str = "excel"
--- src/portfolio/optimisation/schemas.py
+++
@@ -1,6 +0,0 @@
-from utils.utils import EnhancedStrEnum
-
-
-class OptimizationOptions(EnhancedStrEnum):
-    SHARPE_RATIO: str = "SHARPE_RATIO"
-    QUADRATIC_UTILITY: str = "QUADRATIC_UTILITY"
